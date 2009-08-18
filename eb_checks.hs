92478a28ff33a03641be8466a9078b6c0f3a6b99
Raphael Speyer <rspeyer@gmail.com>
2009-08-18 14:57:02 +1000

    added some quickcheck tests for cursor location

--- eb_checks.hs
+++ eb_checks.hs
@@ -1,4 +1,3 @@
-
 import Test.QuickCheck
 import EditBuffer
 
@@ -13,3 +12,15 @@ prop_insert_char_advances n =
   n >= 0 ==> n == x
                where (EditBuffer _ (x,_) contents) = (iterate (insertChar 'a') emptyBuffer) !! n
 
+prop_moveRight_cannot_move_cursor_beyond_last_char n =
+  n >= 1 ==>
+    (n-1, 0) == loc
+      where (EditBuffer _ loc _) = moveRight $ (iterate (insertChar 'a') emptyBuffer) !! n
+
+prop_moveRightOver_can_move_cursor_to_after_last_char n =
+  n >= 0 ==>
+    (n, 0) == loc
+      where (EditBuffer _ loc _) = moveRightOver $ (iterate (insertChar 'a') emptyBuffer) !! n
+
+prop_delete_on_an_empty_line_makes_no_change =
+  emptyBuffer == (deleteChar emptyBuffer)
